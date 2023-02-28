a79233b004f09bb6a537746b89f164eefc512f1d
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-02-28 18:08:59 +0530

    Innitial commit

---
+++ src/main/java/com/example/top/TopApplication.java
@@ -0,0 +1,13 @@
+package com.example.top;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopApplication.class, args);
+	}
+
+}
