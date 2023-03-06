4b25a1ca418b8b9bc212387bbe747fe9370efd76
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-03-06 18:45:13 +0530

    User interface initial commit

---
+++ src/main/java/com/example/top/user/User.java
@@ -0,0 +1,10 @@
+package com.example.top.user;
+
+public interface User {
+
+    void setUsername(String username);
+    String getUsername();
+    void setPassword(String password);
+    String getPassword();
+
+}
