578c2667691e476ff80a20e3fd79def121776a51
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-03-08 16:41:54 +0530

    EmployeeService.java initial commit

---
+++ src/main/java/com/example/top/service/EmployeeService.java
@@ -0,0 +1,81 @@
+package com.example.top.service;
+
+import com.example.top.entity.Employee;
+import com.example.top.entity.Role;
+import com.example.top.repository.EmployeeRepository;
+import com.example.top.repository.RoleRepository;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@ToString
+@AllArgsConstructor
+@Log
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    public boolean addEmployee(Employee employee) {
+        if (employee == null) {
+            log.severe("Cannot add null as an employee");
+            return false;
+        }
+
+        employeeRepository.save(employee);
+
+        var name = employee.getFirstname() + " " + employee.getLastname();
+        log.info("Employee with name '" + name + "' has been added" );
+        return true;
+    }
+
+    public List<Employee> findAllEmployees() {
+        var employees = employeeRepository.findAll();
+
+        log.info("Successfully retrieved all employees");
+        return employees;
+    }
+
+    public void removeEmployee(Employee employee) {
+        employeeRepository.delete(employee);
+
+        var name = employee.getFirstname() + " " + employee.getLastname();
+        log.info("Employee with name '" + name + "' has been deleted");
+    }
+
+    public boolean addRole(Role role) {
+        if (role == null) {
+            log.severe("Cannot add null as a role");
+            return false;
+        }
+
+        roleRepository.save(role);
+        log.info("New role '" + role.getName() + "' is added");
+        return true;
+    }
+
+    public List<Role> findAllRoles() {
+        var roles = roleRepository.findAll();
+
+        log.info("Successfully retrieved all roles");
+        return roles;
+    }
+
+    public void updateRole(Role role, String name) {
+        roleRepository.updateNameByName(role.getName(), name);
+    }
+
+    public void removeRole(Role role) {
+        roleRepository.delete(role);
+
+        log.info("Successfully deleted the role '" + role.getName() + "'") ;
+    }
+}
