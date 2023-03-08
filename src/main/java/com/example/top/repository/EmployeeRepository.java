895a57e688a33e54751881cf131ec7607f35346d
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-03-08 17:00:19 +0530

    EmployeeRepository.java Added remove employee by firstname method

--- src/main/java/com/example/top/repository/EmployeeRepository.java
+++ src/main/java/com/example/top/repository/EmployeeRepository.java
@@ -2,6 +2,12 @@ package com.example.top.repository;
 
 import com.example.top.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    @Modifying
+    @Transactional
+    int removeEmployeeByFirstname(String firstname);
 }
