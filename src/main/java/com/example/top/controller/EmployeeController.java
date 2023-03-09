493410461ac454cf5f0e31dcce9ef2b74aa52c75
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-03-09 21:22:12 +0530

    Added remove method

--- src/main/java/com/example/top/controller/EmployeeController.java
+++ src/main/java/com/example/top/controller/EmployeeController.java
@@ -3,8 +3,10 @@ package com.example.top.controller;
 import com.example.top.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class EmployeeController {
@@ -13,11 +15,18 @@ public class EmployeeController {
     private EmployeeService empService;
 
     @RequestMapping("/employees")
-    public ModelAndView employee() {
+    public ModelAndView getEmployees() {
         var mv = new ModelAndView();
         mv.addObject("employees", empService.findAllEmployees());
         mv.setViewName("admin/admin-employee");
 
         return mv;
+    }
+
+    @GetMapping("/remove-emp")
+    public RedirectView removeEmployee(Long id) {
+        empService.removeEmployee(id);
+
+        return new RedirectView("employees");
     }
 }
