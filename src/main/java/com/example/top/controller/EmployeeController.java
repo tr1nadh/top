36d8e55bf477081b0f55e48d1fb82fe6ba20e46f
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-03-10 12:34:13 +0530

    Added addEmployee method and changed removeEmployee mapping

--- src/main/java/com/example/top/controller/EmployeeController.java
+++ src/main/java/com/example/top/controller/EmployeeController.java
@@ -1,9 +1,11 @@
 package com.example.top.controller;
 
+import com.example.top.entity.Employee;
 import com.example.top.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
@@ -23,9 +25,16 @@ public class EmployeeController {
         return mv;
     }
 
-    @GetMapping("/remove-emp")
+    @GetMapping("/delete-emp")
     public RedirectView removeEmployee(Long id) {
         empService.deleteEmployee(id);
+
+        return new RedirectView("employees");
+    }
+
+    @PostMapping("/add-emp")
+    public RedirectView addEmployee(Employee employee) {
+        empService.addEmployee(employee);
 
         return new RedirectView("employees");
     }
