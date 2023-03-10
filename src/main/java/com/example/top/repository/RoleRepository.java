ca60ea82ef5f68805bb48a052d2952a045613361
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-03-10 10:50:18 +0530

    Removed @Transactional annotation

--- src/main/java/com/example/top/repository/RoleRepository.java
+++ src/main/java/com/example/top/repository/RoleRepository.java
@@ -5,17 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
     @Modifying
-    @Transactional
     @Query(value = "update Role r set r.name = :newName where r.name = :oldName")
     int updateNameByName(String oldName, String newName);
 
     @Modifying
-    @Transactional
     int removeRoleByName(String name);
 }
--- src/main/java/com/example/top/repository/UserRepository.java
+++ src/main/java/com/example/top/repository/UserRepository.java
@@ -4,18 +4,15 @@ import com.example.top.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.transaction.annotation.Transactional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByUsername(String username);
 
-    @Transactional
     @Modifying
     @Query (value = "update User u set u.password = :newPassword where u.username = :username")
     int updatePasswordByUsername(String username, String newPassword);
 
-    @Transactional
     @Modifying
     @Query (value = "update User u set u.username = :newUsername where u.username = :oldUsername")
     int updateUsernameByUsername(String oldUsername, String newUsername);
