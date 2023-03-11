e3c17445f9c71552c3c72884e1624f083f3a57ae
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-03-11 21:34:02 +0530

    Initial commit

---
+++ src/main/java/com/example/top/repository/ServiceRepository.java
@@ -0,0 +1,9 @@
+package com.example.top.repository;
+
+import com.example.top.entity.Service;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ServiceRepository extends JpaRepository<Service, Long> {
+}
