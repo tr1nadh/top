686c843f6a171841d505d5eb21a9d9698deccabf
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-03-11 09:58:08 +0530

    Added update employee by id method

--- src/main/java/com/example/top/repository/EmployeeRepository.java
+++ src/main/java/com/example/top/repository/EmployeeRepository.java
@@ -3,9 +3,16 @@ package com.example.top.repository;
 import com.example.top.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
     @Modifying
     int deleteEmployeeByFirstname(String firstname);
+
+    @Modifying
+    @Query("update Employee e set e.firstname = ?1, e.lastname = ?2, e.role = ?3, " +
+            "e.phoneNo = ?4, e.emailAddress = ?5, e.gender = ?6 where id = ?7")
+    int updateEmployeeById(String firstname, String lastname, String role,
+                           Long phoneNo, String emailAddress, String gender, Long id);
 }
