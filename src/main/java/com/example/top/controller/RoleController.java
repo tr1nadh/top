9db70db029cabae5104005730a37520084fb0c1e
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-03-11 19:37:29 +0530

    Refactored the clas to use RoleService class

--- src/main/java/com/example/top/controller/RoleController.java
+++ src/main/java/com/example/top/controller/RoleController.java
@@ -1,39 +1,30 @@
 package com.example.top.controller;
 
 import com.example.top.entity.Role;
-import com.example.top.repository.RoleRepository;
+import com.example.top.service.RoleService;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 @Log
 public class RoleController {
 
     @Autowired
-    private RoleRepository repository;
+    private RoleService service;
 
     @GetMapping("/add-role")
-    public boolean addRole(Role role) {
-        if (role == null) {
-            log.severe("Cannot add null as a role");
-            return false;
-        }
-
-        repository.save(role);
-
-        log.info("Role with the name '" + role.getName() + "' has been added");
-        return true;
+    public String showAddRole() {
+        return "admin-add-role";
     }
 
-    @GetMapping("/roles")
-    public List<Role> getRoles() {
-        var roles = repository.findAll();
+    @PostMapping("/add-role")
+    public RedirectView addRole(Role role) {
+        service.addRole(role);
 
-        log.info("Successfully retrieved all the roles");
-        return roles;
+        return new RedirectView("add-employee");
     }
 }
