b2e7106cbef0b6b1b19278b8c5da9dd711af669a
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-03-12 15:55:10 +0530

    Initial commit

---
+++ src/main/java/com/example/top/entity/Order.java
@@ -0,0 +1,31 @@
+package com.example.top.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Order {
+
+    @Id
+    @SequenceGenerator(
+            name = "order_seq",
+            sequenceName = "order_seq",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "order_seq"
+    )
+    private Long id;
+    private String customer;
+    private String handleBy;
+    private String work;
+    private int totalAmount;
+    private int amountPaid;
+}
