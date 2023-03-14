6e0dff75abbf6e75af39daab1e6627e974e6b2f0
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-03-14 14:09:07 +0530

    Added role parameter

--- src/main/java/com/example/top/repository/EmployeeRepository.java
+++ src/main/java/com/example/top/repository/EmployeeRepository.java
@@ -1,6 +1,7 @@
 package com.example.top.repository;
 
 import com.example.top.entity.Employee;
+import com.example.top.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -13,6 +14,6 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     @Modifying
     @Query("update Employee e set e.firstname = ?1, e.lastname = ?2, e.role = ?3, " +
             "e.phoneNo = ?4, e.emailAddress = ?5, e.gender = ?6 where id = ?7")
-    int updateEmployeeById(String firstname, String lastname, String role,
+    int updateEmployeeById(String firstname, String lastname, Role role,
                            Long phoneNo, String emailAddress, String gender, Long id);
 }
