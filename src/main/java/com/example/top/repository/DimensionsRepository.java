44ca84e33f460a7adf918bfed7c19c340b84ca0f
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-03-14 14:05:10 +0530

    Deleted

--- src/main/java/com/example/top/repository/DimensionsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.top.repository;
-
-import com.example.top.entity.Dimensions;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DimensionsRepository extends JpaRepository<Dimensions, Long> {
-}
--- src/main/java/com/example/top/repository/EmployeeBillRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.top.repository;
-
-import com.example.top.entity.EmployeeBill;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeBillRepository extends JpaRepository<EmployeeBill, Long> {
-}
