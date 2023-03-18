a0c90ec80f850a3b77e49dcd9758bfd3b5a79aae
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-03-18 17:33:17 +0530

    Deleted unused methods

--- src/main/java/com/example/top/repository/RoleRepository.java
+++ src/main/java/com/example/top/repository/RoleRepository.java
@@ -2,15 +2,6 @@ package com.example.top.repository;
 
 import com.example.top.entity.employee.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
-
-    @Modifying
-    @Query(value = "update Role r set r.name = :newName where r.name = :oldName")
-    int updateNameByName(String oldName, String newName);
-
-    @Modifying
-    int deleteRoleByName(String name);
 }
