0820ca406d766739fd751662007e3e32619c5ca0
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-03-18 17:28:37 +0530

    Removed unnecessary annotations

--- src/main/java/com/example/top/controller/RoleController.java
+++ src/main/java/com/example/top/controller/RoleController.java
@@ -2,7 +2,6 @@ package com.example.top.controller;
 
 import com.example.top.entity.employee.Role;
 import com.example.top.service.RoleService;
-import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +11,6 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
-@Log
 public class RoleController {
 
     @Autowired
