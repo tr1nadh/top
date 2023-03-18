86ca0f6797f1616ec9549201141723f11d5b2dd3
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-03-18 17:32:41 +0530

    Removed repository annotation

--- src/main/java/com/example/top/repository/DimensionsRepository.java
+++ src/main/java/com/example/top/repository/DimensionsRepository.java
@@ -2,8 +2,6 @@ package com.example.top.repository;
 
 import com.example.top.entity.order.Dimensions;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface DimensionsRepository extends JpaRepository<Dimensions, Long> {
 }
--- src/main/java/com/example/top/repository/RoleRepository.java
+++ src/main/java/com/example/top/repository/RoleRepository.java
@@ -4,9 +4,7 @@ import com.example.top.entity.employee.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
     @Modifying
