5467ae5729975f125a4c68b6fdcbb66ab2227963
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-03-20 16:57:48 +0530

    Added view controllers method to manage redirects

--- src/main/java/com/example/top/TopApplication.java
+++ src/main/java/com/example/top/TopApplication.java
@@ -2,12 +2,19 @@ package com.example.top;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @SpringBootApplication
-public class TopApplication {
+public class TopApplication extends WebMvcConfigurationSupport {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TopApplication.class, args);
+	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addRedirectViewController("/", "orders");
 	}
 
 }
