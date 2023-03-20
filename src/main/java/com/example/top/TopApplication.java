00964af6d1ef25d5f070d138b62e7a4415525b43
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-03-20 21:15:26 +0530

    Removed web mvc configuration

--- src/main/java/com/example/top/TopApplication.java
+++ src/main/java/com/example/top/TopApplication.java
@@ -2,19 +2,12 @@ package com.example.top;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @SpringBootApplication
-public class TopApplication extends WebMvcConfigurationSupport {
+public class TopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TopApplication.class, args);
-	}
-
-	@Override
-	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addRedirectViewController("/", "orders");
 	}
 
 }
