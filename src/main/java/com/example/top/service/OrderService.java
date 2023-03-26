4dc443c84cc8b1f660115a5ecb3c2209e139bcb6
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-03-26 16:14:17 +0530

    Refactored

--- src/main/java/com/example/top/service/OrderService.java
+++ src/main/java/com/example/top/service/OrderService.java
@@ -30,7 +30,7 @@ public class OrderService {
         log.info("Order with the id '" + order.getOrderId() + "' has been saved" );
     }
 
-    public Order applyStatus(Order order) {
+    private Order applyStatus(Order order) {
         var serviceStatus = order.getService().getServiceStatus();
         if (serviceStatus == null) return order;
         var paymentStatus = order.getPayment().getPaymentStatus();
@@ -51,13 +51,6 @@ public class OrderService {
         return order;
     }
 
-    public List<Order> findAllOrders() {
-        var orders = repository.findAll();
-
-        log.info("Successfully retrieved all orders");
-        return orders;
-    }
-
     public Order getOrder(Long id) {
         var optionalOrder = repository.findById(id);
 
@@ -68,39 +61,6 @@ public class OrderService {
 
         log.info("Order with the id '" + id + "' has been retrieved");
         return optionalOrder.get();
-    }
-
-    public void deleteOrder(Long id) {
-        var order = getOrder(id);
-        if (order == null) {
-            log.severe("Cannot delete the order with the id '" + id + "' which doesn't exists");
-            return;
-        }
-
-        repository.deleteById(id);
-
-        log.info("Order with the id '" + id + "' has been deleted");
-    }
-
-    public List<Order> findOrdersByCustomerNameContaining(String name) {
-        var orders = repository.findOrdersByCustomerNameContaining(name);
-
-        log.info("Successfully retrieved orders by customer name containing '" + name + "'");
-        return orders;
-    }
-
-    public List<Order> findOrdersByOrderStatus(String status) {
-        var orders = repository.findOrdersByOrderStatus(status);
-
-        log.info("Successfully retrieved orders by order status '" + status + "'");
-        return orders;
-    }
-
-    public List<Order> findOrdersByOrderStatusAndCustomerNameContaining(String status, String name) {
-        var orders = repository.findOrdersByOrderStatusAndCustomerNameContaining(status, name);
-
-        log.info("Successfully retrieved orders by order status '" + status + "' and customer name containing '" + name + "'");
-        return orders;
     }
 
     public List<Order> getOrdersByParams(String search, String orderStatus) {
@@ -118,5 +78,38 @@ public class OrderService {
         else orders = findOrdersByOrderStatusAndCustomerNameContaining(orderStatus, search);
 
         return orders;
+    }
+
+    private List<Order> findOrdersByCustomerNameContaining(String name) {
+        var orders = repository.findOrdersByCustomerNameContaining(name);
+
+        log.info("Successfully retrieved orders by customer name containing '" + name + "'");
+        return orders;
+    }
+
+    private List<Order> findOrdersByOrderStatus(String status) {
+        var orders = repository.findOrdersByOrderStatus(status);
+
+        log.info("Successfully retrieved orders by order status '" + status + "'");
+        return orders;
+    }
+
+    private List<Order> findOrdersByOrderStatusAndCustomerNameContaining(String status, String name) {
+        var orders = repository.findOrdersByOrderStatusAndCustomerNameContaining(status, name);
+
+        log.info("Successfully retrieved orders by order status '" + status + "' and customer name containing '" + name + "'");
+        return orders;
+    }
+
+    public void deleteOrder(Long id) {
+        var order = getOrder(id);
+        if (order == null) {
+            log.severe("Cannot delete the order with the id '" + id + "' which doesn't exists");
+            return;
+        }
+
+        repository.deleteById(id);
+
+        log.info("Order with the id '" + id + "' has been deleted");
     }
 }
