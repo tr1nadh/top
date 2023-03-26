3925a65e882b5fa3e6b6dd0422c141f3467fafdb
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-03-26 13:05:30 +0530

    Removed business logic methods

--- src/main/java/com/example/top/controller/OrderController.java
+++ src/main/java/com/example/top/controller/OrderController.java
@@ -8,7 +8,6 @@ import com.example.top.service.DimensionsService;
 import com.example.top.service.EmployeeService;
 import com.example.top.service.OrderService;
 import com.example.top.service.ServiceTypeService;
-import com.example.top.util.GeneralUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,7 +16,6 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.Arrays;
-import java.util.List;
 
 @Controller
 public class OrderController {
@@ -60,7 +58,7 @@ public class OrderController {
     @GetMapping({"/", "/orders"})
     public ModelAndView getOrders(String search, String orderStatus) {
         var mv = new ModelAndView();
-        mv.addObject("orders", getOrdersByParams(search, orderStatus));
+        mv.addObject("orders", orderService.getOrdersByParams(search, orderStatus));
         mv.addObject("order", new Order());
         mv.addObject("serviceStatus", Arrays.stream(ServiceStatus.values()).toList());
         mv.addObject("paymentStatus", Arrays.stream(PaymentStatus.values()).toList());
@@ -89,22 +87,5 @@ public class OrderController {
         orderService.deleteOrder(id);
 
         return new RedirectView("orders");
-    }
-
-    public List<Order> getOrdersByParams(String search, String orderStatus) {
-        List<Order> orders;
-        if (GeneralUtil.isQualifiedString(orderStatus)) orders = getOrdersBySearchAndOrderStatus(search, orderStatus);
-        else if (!GeneralUtil.isQualifiedString(search)) orders = orderService.findOrdersByOrderStatus("Pending");
-        else orders = orderService.findOrdersByCustomerNameContaining(search);
-
-        return orders;
-    }
-
-    public List<Order> getOrdersBySearchAndOrderStatus(String search, String orderStatus) {
-        List<Order> orders;
-        if (!GeneralUtil.isQualifiedString(search)) orders = orderService.findOrdersByOrderStatus(orderStatus);
-        else orders = orderService.findOrdersByOrderStatusAndCustomerNameContaining(orderStatus, search);
-
-        return orders;
     }
 }
