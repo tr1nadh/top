94e1689bf07a503799bb2e32f07c5f4c48dadd81
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-03-28 17:43:36 +0530

    Added lombok constructor annotations

--- src/main/java/com/example/top/dto/EmployeeDto.java
+++ src/main/java/com/example/top/dto/EmployeeDto.java
@@ -4,11 +4,15 @@ import com.example.top.entity.employee.Role;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class EmployeeDto {
 
     private Long employeeId;
