9494d964e1163461e7da3c8ff055b2ffb629097c
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-03-28 10:23:11 +0530

    Removed validation annotations

--- src/main/java/com/example/top/entity/employee/Employee.java
+++ src/main/java/com/example/top/entity/employee/Employee.java
@@ -1,9 +1,6 @@
 package com.example.top.entity.employee;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 @Entity
@@ -26,25 +23,24 @@ public class Employee {
             generator = "employee_seq"
     )
     private Long employeeId;
+
     @Column(unique = true)
-    @NotEmpty(message = "First name cannot be empty")
     private String firstname;
-    @NotEmpty(message = "Last name cannot be empty")
+
     private String lastname;
+
     @OneToOne(
             cascade = {CascadeType.PERSIST, CascadeType.MERGE}
     )
     @JoinColumn(name = "role_id")
     private Role role;
+
     @Column(unique = true)
-    @Pattern(regexp="(^$|[0-9]{10})", message = "Phone no must be 10 digits")
-    @NotEmpty(message = "Phone no cannot be empty")
     private String phoneNo;
-    @Email(message = "Must be a valid email")
-    @NotEmpty(message = "Email cannot be empty")
+
     @Column(unique = true)
     private String emailAddress;
-    @NotEmpty(message = "Gender cannot be empty")
+
     private String gender;
 
 }
