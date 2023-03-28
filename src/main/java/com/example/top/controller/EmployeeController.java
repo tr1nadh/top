e26c13708ad7554b6673751bf7f477db40c45a33
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-03-28 17:43:04 +0530

    Added dto object to save employee method

--- src/main/java/com/example/top/controller/EmployeeController.java
+++ src/main/java/com/example/top/controller/EmployeeController.java
@@ -1,14 +1,17 @@
 package com.example.top.controller;
 
+import com.example.top.dto.EmployeeDto;
 import com.example.top.entity.employee.Employee;
 import com.example.top.service.EmployeeService;
 import com.example.top.service.RoleService;
+import com.example.top.util.Mapper;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -35,14 +38,14 @@ public class EmployeeController {
     }
 
     @PostMapping({"/save-employee", "/update-employee"})
-    public ModelAndView saveEmployee(@Valid Employee employee, BindingResult bindingResult, ModelMap model) {
+    public ModelAndView saveEmployee(@Valid @ModelAttribute("employee") EmployeeDto employee, BindingResult bindingResult, ModelMap model) {
         if (bindingResult.hasErrors()) {
             model.addAttribute("employee", employee);
             model.addAttribute("roles", roleService.findAllRoles());
             return new ModelAndView("employee/save-employee", model);
         }
 
-        empService.saveEmployee(employee);
+        empService.saveEmployee(Mapper.map(employee, new Employee()));
 
         return new ModelAndView("redirect:/employees");
     }
