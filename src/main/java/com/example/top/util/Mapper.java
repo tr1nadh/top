f5b0c0d973c407f00dbcab763fc481090bc4a0ed
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-03-30 12:32:00 +0530

    Added list method to map list of objects

--- src/main/java/com/example/top/util/Mapper.java
+++ src/main/java/com/example/top/util/Mapper.java
@@ -3,11 +3,17 @@ package com.example.top.util;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class Mapper {
 
     public static<F, T> T map(F fromObj, T toObj) {
         BeanUtils.copyProperties(fromObj, toObj);
         return toObj;
+    }
+
+    public static<F, T> void mapList(List<F> listOfFromObj, T toObj) {
+        BeanUtils.copyProperties(listOfFromObj, toObj);
     }
 }
