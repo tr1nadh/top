801374d3adadc71a92385a373bffd0432e6ed2ea
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-03-31 09:10:12 +0530

    Added builder annotation

--- src/main/java/com/example/top/dto/OrderDto.java
+++ src/main/java/com/example/top/dto/OrderDto.java
@@ -2,12 +2,14 @@ package com.example.top.dto;
 
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class OrderDto {
 
     private Long orderId;
