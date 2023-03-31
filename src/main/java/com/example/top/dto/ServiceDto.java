92cb866a0474d0e37fdd505cee84522d8e5f53cd
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-03-31 09:10:23 +0530

    Added builder annotation

--- src/main/java/com/example/top/dto/ServiceDto.java
+++ src/main/java/com/example/top/dto/ServiceDto.java
@@ -6,12 +6,14 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class ServiceDto {
 
     private ServiceType serviceType;
