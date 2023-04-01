c9b56b111540c881412ff5bd3816300d9336b52d
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-04-01 13:22:19 +0530

    Added roledto field

--- src/main/java/com/example/top/dto/EmployeeDto.java
+++ src/main/java/com/example/top/dto/EmployeeDto.java
@@ -1,6 +1,5 @@
 package com.example.top.dto;
 
-import com.example.top.entity.employee.Role;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
@@ -22,7 +21,7 @@ public class EmployeeDto {
     @NotEmpty(message = "Last name cannot be empty")
     private String lastname;
 
-    private Role role;
+    private RoleDto role;
 
     @Pattern(regexp="(^$|[0-9]{10})", message = "Phone no must be 10 digits")
     @NotEmpty(message = "Phone no cannot be empty")
