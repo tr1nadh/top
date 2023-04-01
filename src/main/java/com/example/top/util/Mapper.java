f5aec7672cfdde83c78d217b26e866c64aff5834
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-04-01 10:10:43 +0530

    Removed unused list method

--- src/main/java/com/example/top/util/Mapper.java
+++ src/main/java/com/example/top/util/Mapper.java
@@ -2,23 +2,10 @@ package com.example.top.util;
 
 import org.springframework.beans.BeanUtils;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Mapper {
 
     public static<F, T> T map(F fromObj, T toObj) {
         BeanUtils.copyProperties(fromObj, toObj);
         return toObj;
-    }
-
-    public static<F, T> List<T> mapList(List<F> listOfFromObj, T toObj) {
-        var list = new ArrayList<T>();
-        for (var obj : listOfFromObj) {
-            var mappedObj = map(obj, toObj);
-            list.add(mappedObj);
-        }
-
-        return list;
     }
 }
