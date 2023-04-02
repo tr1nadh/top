fe6704b3c94ad9a18774ef332ba965ddec73634a
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-04-02 15:28:39 +0530

    Added map method to convert dto to domain

--- src/main/java/com/example/top/util/mapper/EmployeeMapper.java
+++ src/main/java/com/example/top/util/mapper/EmployeeMapper.java
@@ -2,11 +2,20 @@ package com.example.top.util.mapper;
 
 import com.example.top.dto.EmployeeDto;
 import com.example.top.entity.employee.Employee;
+import com.example.top.entity.employee.Role;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class EmployeeMapper {
+
+    public static Employee map(EmployeeDto employee) {
+        var mappedEmp = Mapper.map(employee, new Employee());
+        var mappedRole = Mapper.map(employee.getRole(), new Role());
+        mappedEmp.setRole(mappedRole);
+
+        return mappedEmp;
+    }
 
     public static List<EmployeeDto> mapList(List<Employee> employees) {
         var list = new ArrayList<EmployeeDto>();
