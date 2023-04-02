8be2ab6c63688df82768ab8daab695a7c9932435
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-04-02 18:26:04 +0530

    Roll back to prev commit

--- src/main/java/com/example/top/service/OrderService.java
+++ src/main/java/com/example/top/service/OrderService.java
@@ -1,7 +1,9 @@
-package com.example.top.service.order;
+package com.example.top.service;
 
 import com.example.top.entity.order.Order;
 import com.example.top.enums.OrderStatus;
+import com.example.top.enums.PaymentStatus;
+import com.example.top.enums.ServiceStatus;
 import com.example.top.repository.OrderRepository;
 import com.example.top.util.GeneralUtil;
 import lombok.extern.java.Log;
@@ -12,7 +14,7 @@ import java.util.List;
 
 @Service
 @Log
-public class OrderService extends OrderUpdateService {
+public class OrderService {
 
     @Autowired
     private OrderRepository repository;
@@ -76,6 +78,58 @@ public class OrderService extends OrderUpdateService {
 
         log.info("Successfully retrieved orders by order status '" + status + "' and customer name containing '" + name + "'");
         return orders;
+    }
+
+    public void updateAmount(Long orderId, int addAmount, int removeAmount) {
+        var dbOrder = getOrder(orderId);
+        var prevAm = dbOrder.getPayment().getAmountPaid();
+        if (addAmount != 0) {
+            if ((prevAm + addAmount) > dbOrder.getPayment().getTotalAmount()) {
+                log.severe("Cannot add more then enough amount");
+                return;
+            }
+            dbOrder.getPayment().setAmountPaid(prevAm + addAmount);
+        }
+        else if (removeAmount != 0) {
+            if ((prevAm - removeAmount) < 0) {
+                log.severe("Cannot remove the amount");
+                return;
+            }
+            dbOrder.getPayment().setAmountPaid(prevAm - removeAmount);
+        }
+
+        updateAmountStatus(dbOrder);
+        saveOrder(dbOrder);
+    }
+
+    private static void updateAmountStatus(Order dbOrder) {
+        if (dbOrder.getPayment().getAmountPaid() == dbOrder.getPayment().getTotalAmount()) {
+            dbOrder.getPayment().setPaymentStatus(PaymentStatus.PAID.toString());
+            updateOrderStatus(dbOrder);
+        } else if (!dbOrder.getPayment().getPaymentStatus().equals(PaymentStatus.UNPAID.toString())) {
+            dbOrder.getPayment().setPaymentStatus(PaymentStatus.UNPAID.toString());
+            updateOrderStatus(dbOrder);
+        }
+    }
+
+    public void updateServiceStatus(Long orderId, String serviceStatus) {
+        var dbOrder = getOrder(orderId);
+        if (serviceStatus != null) {
+            dbOrder.getService().setServiceStatus(serviceStatus);
+            updateOrderStatus(dbOrder);
+        }
+        saveOrder(dbOrder);
+    }
+
+    private static void updateOrderStatus(Order dbOrder) {
+        var serviceStatus = dbOrder.getService().getServiceStatus();
+        var paymentStatus = dbOrder.getPayment().getPaymentStatus();
+
+        if (serviceStatus.equals(ServiceStatus.COMPLETED.toString()) && paymentStatus.equals(PaymentStatus.PAID.toString())) {
+            dbOrder.setOrderStatus(OrderStatus.COMPLETED.toString());
+        } else if (!dbOrder.getOrderStatus().equals(OrderStatus.PENDING.toString())) {
+            dbOrder.setOrderStatus(OrderStatus.PENDING.toString());
+        }
     }
 
     public void cancelOrder(Long id) {
