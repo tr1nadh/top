9c75dad65980ee4cf4913ba15ff2cc45c2a2a938
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-04-02 15:28:15 +0530

    Initial commit

---
+++ src/main/java/com/example/top/converter/RoleDtoConverter.java
@@ -0,0 +1,21 @@
+package com.example.top.converter;
+
+import com.example.top.dto.RoleDto;
+import com.example.top.service.RoleService;
+import com.example.top.util.mapper.Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RoleDtoConverter implements Converter<String, RoleDto> {
+
+    @Autowired
+    private RoleService service;
+
+    @Override
+    public RoleDto convert(String source) {
+        var id = Long.parseLong(source);
+        return Mapper.map(service.getRole(id), new RoleDto());
+    }
+}
