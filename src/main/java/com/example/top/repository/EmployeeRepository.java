fd98a7090ceccd71be40ee7fdea32ad26e80d45c
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-04-04 15:08:31 +0530

    Added new find all method with exclusion

--- src/main/java/com/example/top/repository/EmployeeRepository.java
+++ src/main/java/com/example/top/repository/EmployeeRepository.java
@@ -3,7 +3,10 @@ package com.example.top.repository;
 import com.example.top.entity.employee.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
     Employee findEmployeeByUsername(String username);
+    List<Employee> findByRoleNameNotIn(List<String> names);
 }
