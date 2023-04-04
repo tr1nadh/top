5d22b43c7babb58d0341e9a8c171a65b1594c843
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-04-04 14:16:54 +0530

    Added find by username new method

--- src/main/java/com/example/top/repository/EmployeeRepository.java
+++ src/main/java/com/example/top/repository/EmployeeRepository.java
@@ -4,4 +4,6 @@ import com.example.top.entity.employee.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    Employee findEmployeeByUsername(String username);
 }
