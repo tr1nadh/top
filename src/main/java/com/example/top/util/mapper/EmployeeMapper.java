738552fc712aa7ed892f9a5e207cd20c8f29824b
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-04-19 11:27:37 +0530

    Added new method to only map employee info

--- src/main/java/com/example/top/util/mapper/EmployeeMapper.java
+++ src/main/java/com/example/top/util/mapper/EmployeeMapper.java
@@ -10,6 +10,14 @@ import java.util.List;
 
 public class EmployeeMapper {
 
+    public static Employee mapInfo(EmployeeDto employeeDto) {
+        var mappedEmp = Mapper.map(employeeDto, new Employee());
+        var mappedRole = Mapper.map(employeeDto.getRole(), new Role());
+        mappedEmp.setRole(mappedRole);
+
+        return mappedEmp;
+    }
+
     public static Employee map(EmployeeDto employeeDto) {
         var mappedEmp = Mapper.map(employeeDto, new Employee());
         var mappedRole = Mapper.map(employeeDto.getRole(), new Role());
