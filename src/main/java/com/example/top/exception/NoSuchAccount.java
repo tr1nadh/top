00709e4f9ee2f3e4f01bec004857e1f1423694ae
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-04-29 09:53:07 +0530

    Deleted

--- src/main/java/com/example/top/exception/NoSuchAccount.java
+++
@@ -1,9 +0,0 @@
-package com.example.top.exception;
-
-public class NoSuchAccount extends RuntimeException {
-
-    public NoSuchAccount(String message) {
-        super(message);
-    }
-
-}
--- src/main/java/com/example/top/exception/UnknownIdException.java
+++
@@ -1,7 +0,0 @@
-package com.example.top.exception;
-
-public class UnknownIdException extends RuntimeException {
-    public UnknownIdException(String message) {
-        super(message);
-    }
-}
