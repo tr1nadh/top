2c59423cea4d7f78a4b00fd902c9226d50381083
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-05-26 21:20:09 +0530

    Added toast error handeling

--- src/main/java/com/example/top/controller/RoleController.java
+++ src/main/java/com/example/top/controller/RoleController.java
@@ -68,4 +68,10 @@ public class RoleController extends AController {
         attributes.addFlashAttribute("alertMessage", message);
         return new RedirectView("view");
     }
+
+    @RequestMapping("/error")
+    public ModelAndView error(String alertMessage, RedirectAttributes attributes) {
+        attributes.addFlashAttribute("alertMessage", alertMessage);
+        return new ModelAndView("forward:view");
+    }
 }
