3c38ac611c4bcbbaca16787edc9f55b7a25cfcd1
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-06-09 21:05:33 +0530

    Some tests

--- src/test/java/com/example/top/TopApplicationTests.java
+++ src/test/java/com/example/top/TopApplicationTests.java
@@ -1,13 +1,51 @@
 package com.example.top;
 
+import com.example.top.enums.OrderStatus;
+import com.example.top.repository.OrderRepository;
+import com.example.top.service.RoleService;
+import com.example.top.service.order.OrderService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TopApplicationTests {
 
+	@Autowired
+	private RoleService service;
+
+	@Autowired
+	private OrderRepository repository;
+
+	@Autowired
+	private OrderService orderService;
+
 	@Test
-	void contextLoads() {
+	void changeRolesNames() {
+		for (var role : service.findAllRoles()) {
+			var roleName = role.getName();
+			var capitalRoleName = roleName.toUpperCase();
+			var prefixedRole = capitalRoleName.substring(0, 1).toUpperCase() + capitalRoleName.substring(1).toLowerCase();
+			System.out.println(prefixedRole);
+			role.setName(prefixedRole);
+			service.saveRole(role);
+		}
+	}
+
+	@Test
+	void getOrdersByOrderStatusAndHandleBy() {
+		for (var order : repository.findOrdersByOrderStatusAndHandleByFullName(OrderStatus.PENDING.toString(), "emp1")) {
+			System.out.println(order);
+		}
+	}
+
+	@Test
+	void getOrdersByOrderStatusAndHandleByAndCustomerNameContaining() {
+		for (var order : repository.findOrdersByOrderStatusAndHandleByFullNameAndCustomerNameContaining(
+				OrderStatus.PENDING.toString(), "emp1", "May"
+		)) {
+			System.out.println(order);
+		}
 	}
 
 }
