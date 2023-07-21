14ed18037c7d1231858b6b0684ace5a57bebbeda
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-07-21 10:48:08 +0530

    Deleted unused class

--- src/main/java/com/example/top/exception/DuplicateException.java
+++
@@ -1,8 +0,0 @@
-package com.example.top.exception;
-
-public class DuplicateException extends RuntimeException {
-
-    public DuplicateException(String message) {
-        super(message);
-    }
-}
