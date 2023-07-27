21668d247f61683d8713f26339fb29c6bf36da24
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-07-27 16:19:55 +0530

    Refactored

--- src/main/java/com/example/top/service/ServiceHelper.java
+++ src/main/java/com/example/top/service/ServiceHelper.java
@@ -2,6 +2,7 @@ package com.example.top.service;
 
 import com.example.top.entity.employee.Account;
 import com.example.top.repository.AccountRepository;
+import com.example.top.security.userdetails.EmployeeAccountDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -13,9 +14,12 @@ public class ServiceHelper {
     @Autowired
     private AccountRepository repository;
 
-    public Account getCurrentLoggedInUserDetails() {
+    public EmployeeAccountDetails getCurrentLoggedInUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        var employeeAccountDetails = (com.example.top.security.userdetails.EmployeeAccountDetails) authentication.getPrincipal();
-        return repository.findAccountByUsername(employeeAccountDetails.getUsername());
+        return (EmployeeAccountDetails) authentication.getPrincipal();
+    }
+
+    public Account getCurrentLoggedInUserDetails() {
+        return repository.findAccountByUsername(getCurrentLoggedInUser().getUsername());
     }
 }
