0719b517bec2fe8560d786736ec625ae16dec503
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-07-27 16:18:54 +0530

    Added builder annotation

--- src/main/java/com/example/top/dto/ResponseDto.java
+++ src/main/java/com/example/top/dto/ResponseDto.java
@@ -1,8 +1,10 @@
 package com.example.top.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class ResponseDto {
     private boolean success;
     private String message;
