8f0dffd2b11f9330a83c7ac0aa0309f6c37ca282
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-07-28 20:17:58 +0530

    Refactored

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,17 +1,16 @@
+function showAlertToast(alertMessage) {
+  document.getElementById("alertMessage").innerHTML = alertMessage;
+  const alertToast = document.getElementById("alertToast");
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(alertToast);
+  document.addEventListener("DOMContentLoaded", () => {
+    toastBootstrap.show();
+  });
+}
 
-    function showAlertToast(alertMessage) {
-        document.getElementById('alertMessage').innerHTML = alertMessage;
-        const alertToast = document.getElementById('alertToast')
-        const toastBootstrap = bootstrap.Toast.getOrCreateInstance(alertToast)
-        document.addEventListener('DOMContentLoaded', () => {
-            toastBootstrap.show()
-        })
-    }
+function checkForToastAlert(alertMessage) {
+  if (alertMessage) {
+    showAlertToast(alertMessage);
+  }
+}
 
-    function checkForToastAlert(alertMessage) {
-        if (alertMessage) {
-            showAlertToast(alertMessage);
-        }
-    }
-
-    checkForToastAlert(alertMessage);
+checkForToastAlert(alertMessage);
--- src/main/resources/static/js/view-script.js
+++ src/main/resources/static/js/view-script.js
@@ -1,8 +1,8 @@
 function confirmDelete(link) {
-    let text = "Are you sure you want to delete?";
-    if (confirm(text) == true) {
-      window.location.replace(link);
-    }
+  let text = "Are you sure you want to delete?";
+  if (confirm(text) == true) {
+    window.location.replace(link);
+  }
 }
 
 function submitForm(id) {
