fcd3ebaee50f68207ecaeade4ac9aab23f8b1360
trinadh <53462309+tr1nadh@users.noreply.github.com>
2024-04-28 12:43:44 +0530

    Added swing web window for the application

--- src/main/java/com/example/top/TopApplication.java
+++ src/main/java/com/example/top/TopApplication.java
@@ -1,15 +1,20 @@
 package com.example.top;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+import java.io.IOException;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
 public class TopApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TopApplication.class, args);
+	public static void main(String[] args) throws IOException {
+		var ctx = new SpringApplicationBuilder(TopApplication.class)
+				.headless(false).run(args);
+
+		TopSwing.run();
 	}
 
 }
