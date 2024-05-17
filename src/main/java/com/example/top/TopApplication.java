a4f76241a1376870038c9e93acf5bed56a81e88b
trinadh <53462309+tr1nadh@users.noreply.github.com>
2024-05-17 18:07:39 +0530

    Added option to run in swing window or as an webserver

--- src/main/java/com/example/top/TopApplication.java
+++ src/main/java/com/example/top/TopApplication.java
@@ -1,5 +1,6 @@
 package com.example.top;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -11,6 +12,19 @@ import java.io.IOException;
 public class TopApplication {
 
 	public static void main(String[] args) throws IOException {
+		launchWithSwing(false, args);
+	}
+
+	private static void launchWithSwing(boolean run, String[] args) {
+		if (run) {
+			runWithSwing(args);
+			return;
+		}
+
+		SpringApplication.run(TopApplication.class, args);
+	}
+
+	private static void runWithSwing(String[] args) {
 		var ctx = new SpringApplicationBuilder(TopApplication.class)
 				.headless(false).run(args);
 
